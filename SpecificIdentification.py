a816f995d45e371a9cf36620fe1ec73bf96971f0
mapardon <mathieu.pardon@ulb.be>
2024-09-05 02:04:58 +0200

    Added BaggingClassifier methods

--- SpecificIdentification.py
+++ SpecificIdentification.py
@@ -22,25 +22,25 @@ class ModelIdentificationSpecific(ModelIdentification):
             for n in [50, 100, 200, 300, 400, 500]:
                 gbc = GradientBoostingClassifier(loss="log_loss", n_estimators=n)
                 auc = self.parametric_identification_cv(gbc, False)
-                self.candidates.append(Candidate(gbc, auc, "n_estimators={}".format(n), False))
+                self.candidates.append(Candidate(gbc, auc, ["n_estimators={}".format(n)], False))
 
         elif par == "subsample":
             for s in [0.1, 0.5, 0.75, 0.9, 1.0]:
                 gbc = GradientBoostingClassifier(loss="log_loss", subsample=s)
                 auc = self.parametric_identification_cv(gbc, False)
-                self.candidates.append(Candidate(gbc, auc, "subsample={}".format(s), False))
+                self.candidates.append(Candidate(gbc, auc, ["subsample={}".format(s)], False))
 
         elif par == "min_sample_split":
             for mss in [2, 3, 5, 10]:
                 gbc = GradientBoostingClassifier(loss="log_loss", min_samples_split=mss)
                 auc = self.parametric_identification_cv(gbc, False)
-                self.candidates.append(Candidate(gbc, auc, "min_sample_split={}".format(mss), False))
+                self.candidates.append(Candidate(gbc, auc, ["min_sample_split={}".format(mss)], False))
 
         elif par == "max_depth":
             for max_depth in [2, 3, 4, 5, 10, 20]:
                 gbc = GradientBoostingClassifier(loss="log_loss", max_depth=max_depth)
                 auc = self.parametric_identification_cv(gbc, False)
-                self.candidates.append(Candidate(gbc, auc, "max_depth={}".format(max_depth), False))
+                self.candidates.append(Candidate(gbc, auc, ["max_depth={}".format(max_depth)], False))
 
         elif par == "init":
             pass
@@ -50,51 +50,53 @@ class ModelIdentificationSpecific(ModelIdentification):
             for mxi in [50, 100, 500, 1000, 5000, 10**4, 10**5, 10**6]:
                 hgb = HistGradientBoostingClassifier(loss="log_loss", max_iter=mxi)
                 auc = self.parametric_identification_cv(hgb, False)
-                self.candidates.append(Candidate(hgb, auc, "max_iter={}".format(mxi), False))
+                self.candidates.append(Candidate(hgb, auc, ["max_iter={}".format(mxi)], False))
 
         elif par == "l2":
             for l2 in [0.0, 0.05, 0.1, 0.3, 0.5, 1.0, 5.0]:
                 hgb = HistGradientBoostingClassifier(loss="log_loss", l2_regularization=l2)
                 auc = self.parametric_identification_cv(hgb, False)
-                self.candidates.append(Candidate(hgb, auc, "l2_reg={}".format(l2), False))
+                self.candidates.append(Candidate(hgb, auc, ["l2_reg={}".format(l2)], False))
 
         elif par == "min_samples_leaf":
             for msl in [10, 20, 30, 40, 50]:
                 hgb = HistGradientBoostingClassifier(loss="log_loss", min_samples_leaf=msl)
                 auc = self.parametric_identification_cv(hgb, False)
-                self.candidates.append(Candidate(hgb, auc, "min_sample_leaf={}".format(msl), False))
+                self.candidates.append(Candidate(hgb, auc, ["min_sample_leaf={}".format(msl)], False))
 
         elif par == "max_features":
             for mf in [0.5, 0.75, 0.9, 1.0]:
                 hgb = HistGradientBoostingClassifier(loss="log_loss", max_features=mf)
                 auc = self.parametric_identification_cv(hgb, False)
-                self.candidates.append(Candidate(hgb, auc, "max_features={}".format(mf), False))
+                self.candidates.append(Candidate(hgb, auc, ["max_features={}".format(mf)], False))
 
     def bc(self, par):
-        for e in [None, DecisionTreeClassifier(criterion="log_loss", splitter="best"),
-                  DecisionTreeClassifier(criterion="log_loss", splitter="random"),
-                  LogisticRegression(max_iter=100000),
-                  GradientBoostingClassifier(loss="log_loss", n_estimators=300, subsample=0.75,
-                                             min_samples_split=2, max_depth=4)][:-1]:
+        # TODO: manage multi-prametricity
+        for e, n in zip([None, DecisionTreeClassifier(criterion="log_loss", splitter="best"),
+                         DecisionTreeClassifier(criterion="log_loss", splitter="random"),
+                         LogisticRegression(max_iter=100000),
+                         GradientBoostingClassifier(loss="log_loss", n_estimators=300, subsample=0.75,
+                                                     min_samples_split=2, max_depth=4)], ['None', 'DTC-best',
+                                                                                          'DTC-random', 'LR', 'GBC']):
 
             if par == "max_features":
                 for mf in [0.5, 0.75, 1.0]:
                     bc = BaggingClassifier(estimator=e, max_features=mf)
                     auc = self.parametric_identification_cv(bc, False)
-                    self.candidates.append(Candidate(bc, auc, "estimator={}, max_features={}".format(type(e), mf), False))
+                    self.candidates.append(Candidate(bc, auc, ["max_features={}".format(mf), n], False))
 
             elif par == "oob_score":
                 for oob in [False, True]:
                     bc = BaggingClassifier(estimator=e, oob_score=oob)
                     auc = self.parametric_identification_cv(bc, False)
-                    self.candidates.append(Candidate(bc, auc, "estimator={}, oob_score={}".format(type(e), oob), False))
+                    self.candidates.append(Candidate(bc, auc, ["oob_score={}".format(oob), n], False))
 
             elif par == "n_estimators":
                 ns = [10, 15, 20] if type(e) is GradientBoostingClassifier() else [10, 20, 50, 100, 200]
-                for n in ns:
-                    bc = BaggingClassifier(estimator=e, n_estimators=n)
+                for ne in ns:
+                    bc = BaggingClassifier(estimator=e, n_estimators=ne)
                     auc = self.parametric_identification_cv(bc, False)
-                    self.candidates.append(Candidate(bc, auc, "estimator={}, n_estimators={}".format(type(e), n), False))
+                    self.candidates.append(Candidate(bc, auc, ["n_estimators={}".format(ne), n], False))
 
 
 class SpecificIdentification:
@@ -115,11 +117,12 @@ class SpecificIdentification:
     def specific_identification(self, variant, model, par):
         final_train_sets, final_test_sets = list(), list()
         for i in range(self.exp_rounds):
-            final_train_sets.append((pd.read_pickle("serialized_df/trs_{}_features_{}".format(variant, str(i)))[:500],
-                                     pd.read_pickle("serialized_df/trs_{}_labels_{}".format(variant, str(i)))[:500]))
-            final_test_sets.append((pd.read_pickle("serialized_df/tss_{}_features_{}".format(variant, str(i)))[:500],
-                                    pd.read_pickle("serialized_df/tss_{}_labels_{}".format(variant, str(i)))[:500]))
+            final_train_sets.append((pd.read_pickle("serialized_df/trs_{}_features_{}".format(variant, str(i)))[:300],
+                                     pd.read_pickle("serialized_df/trs_{}_labels_{}".format(variant, str(i)))[:300]))
+            final_test_sets.append((pd.read_pickle("serialized_df/tss_{}_features_{}".format(variant, str(i)))[:300],
+                                    pd.read_pickle("serialized_df/tss_{}_labels_{}".format(variant, str(i)))[:300]))
 
+        # get experiment results
         candidates = list()
         for i in range(self.exp_rounds):
             mi = ModelIdentificationSpecific(*final_train_sets[i], *final_test_sets[i], cv_folds=5, verbose=True)
@@ -128,36 +131,53 @@ class SpecificIdentification:
             mi.model_selection(n=1000)
             candidates += mi.model_testing()
 
+        # print all candidates
         print("\nFinal {} candidates".format(variant))
         for c in sorted(candidates, reverse=True, key=lambda x: x.auc):
             print(c)
 
+        # group results by parameters for plotting
+        candidates.sort(key=lambda x: x.pars[1])
+        if len(candidates[0].pars) == 1:
+            candidates = [candidates]
+        elif len(candidates[0].pars) == 2:
+            candidates2 = [[]]
+            for i in range(1, len(candidates) - 1):
+                if candidates[i].pars[1] != candidates[i-1].pars[1]:
+                    candidates2.append(list())
+                candidates2[-1].append(candidates[i])
+            candidates = candidates2
+
         # plotting bc its cool
-        candidates_num = list()
-        for c in candidates:
-            c.pars = float(c.pars.split('=')[1])
-            candidates_num.append(c)
-        candidates_num.sort(key=lambda x: x.pars)
+        for category in candidates:
+            category_num = list()
+            for c in category:
+                num_par = c.pars[0].split('=')[-1]
+                num_par = float(num_par) if num_par not in ["True", "False"] else {"True": 1.0, "False": 0.0}[num_par]
+                category_num.append(Candidate(c.model, c.auc, num_par, c.is_reg_model))
+            category_num.sort(key=lambda x: x.pars)
 
-        print(candidates_num)
-        x, y = list(), list()
-        for i in range(0, len(candidates_num), self.exp_rounds):
-            x.append(candidates_num[i].pars)
-            y.append(statistics.mean([candidates_num[j].auc for j in range(i, min(len(candidates_num), i+self.exp_rounds))]))
-        print(x, y)
-        fig, ax = plt.subplots()
-        ax.plot(x, y)
+            x, y = list(), list()
+            for i in range(0, len(category_num), self.exp_rounds):
+                x.append(category_num[i].pars)
+                y.append(statistics.mean([category_num[j].auc for j in range(i, min(len(category_num), i+self.exp_rounds))]))
+            fig, ax = plt.subplots()
+            ax.plot(x, y)
 
-        ax.set(xlabel=par, ylabel='AUC', title='Bigger is better ({}, {})?'.format(model, variant))
-        ax.grid()
-        plt.show()
-        #plt.savefig("{}-{}-{}.png".format(variant, model, par))
+            try:
+                ax.set(xlabel=par, ylabel='AUC', title='Bigger is better ({}, {}, {})?'.format(model, variant,
+                                                                                               category[0].pars[-1]))
+            except Exception as e:
+                print('>>>', e, category)
+            ax.grid()
+            #plt.show()
+            plt.savefig("figures/{}-{}-{}-{}.png".format(variant, model, par, category[0].pars[-1]))
 
 
 def multi_proc():
     #confs = [("gbc", "n_estimators",), ("gbc", "subsample",), ("gbc", "min_sample_split",), ("gbc", "max_depth",)]
-    confs = [("bc", "max_features",), ("bc", "oob_score",), ("bc", "n_estimators",)]
-    procs = [Process(target=SpecificIdentification(1, ("h1n1",), (c,)).main) for c in confs]
+    confs = [("bc", "max_features",), ("bc", "n_estimators",)]
+    procs = [Process(target=SpecificIdentification(5, ("h1n1",), (c,)).main) for c in confs]
 
     for p in procs:
         p.start()
